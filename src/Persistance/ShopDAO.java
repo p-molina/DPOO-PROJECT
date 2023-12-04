72b23e6ada6b1e9292b7b4e89c8b7c889b3f0b6f
jofreturacentelles <jofrejtc@gmail.com>
2023-12-04 19:21:13 +0100

    funció ShopDAO

--- src/Persistance/ShopDAO.java
+++ src/Persistance/ShopDAO.java
@@ -1,12 +1,13 @@
 package Persistance;
 
 import Bussines.Entities.Shop;
-import org.json.JSONArray;
-import org.json.JSONObject;
+import com.google.gson.Gson;
+import com.google.gson.GsonBuilder;
+import com.google.gson.reflect.TypeToken;
 
-import java.io.File;
-import java.io.FileNotFoundException;
-import java.io.IOException;
+
+import java.io.*;
+import java.lang.reflect.Type;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
@@ -17,58 +18,64 @@ public class ShopDAO {
 
     private final static Path path = Path.of("files/shops.json");
     public ShopDAO(){}
-
-    public void checkFile() throws FileNotFoundException {
+    public static void checkFile() throws FileNotFoundException {
         File file = path.toFile();
 
         if (!file.exists()) {
-            throw new FileNotFoundException("ERROR: The file \"shops.json\" was not found.");
-        }
-    }
-    public static List<Shop> cargarTiendasDesdeArchivo() {
-        List<Shop> listaTiendas = new ArrayList<>();
-        try {
-            String contenido = new String(Files.readAllBytes(Paths.get(rutaArchivoTiendas)));
-            JSONArray jsonTiendas = new JSONArray(contenido);
-            for (int i = 0; i < jsonTiendas.length(); i++) {
-                JSONObject jsonTienda = jsonTiendas.getJSONObject(i);
-                Shop tienda = Shop.shopFromJSON(jsonTienda);
-                listaTiendas.add(tienda);
+            try {
+                file.createNewFile(); // Crear el archivo si NO existe
+            } catch (IOException e) {
+                e.printStackTrace();
             }
-        } catch (IOException | JSONException x) {
-            //dotheexeption
         }
-        return listaTiendas;
-    }
-    public static List<Shop> readAll() {
-        return cargarTiendasDesdeArchivo();
     }
 
-    public static void guardarTiendasEnArchivo(List<Shop> tiendas) {
-        JSONArray jsonTiendas = new JSONArray();
-        for (Shop tienda : tiendas) {
-            jsonTiendas.put(tienda.shopToJSON());
-        }
-        try {
-            Files.write(Paths.get(rutaArchivoTiendas), jsonTiendas.toString().getBytes());
+    public static void saveAllShops(List<Shop> shops) {
+        Gson gson = new GsonBuilder().setPrettyPrinting().create();
+
+        try (FileWriter fileWriter = new FileWriter("files/shops.json")) {
+            gson.toJson(shops, fileWriter);
         } catch (IOException e) {
             e.printStackTrace();
         }
     }
-
-    public static void addShopToFile(Shop tienda) {
-        List<Shop> tiendas = cargarTiendasDesdeArchivo();
-        tiendas.add(tienda);
-        guardarTiendasEnArchivo(tiendas);
+    public static void addShop(Shop shop) {
+        try {
+            checkFile(); // Comprobar si el archivo existe
+        } catch (FileNotFoundException e) {
+            throw new RuntimeException(e);
+        }
+        List<Shop> existingShops = loadAllShops();
+        existingShops.add(shop);
+        saveAllShops(existingShops);
     }
+    private static List<Shop> loadAllShops() {
+        List<Shop> shops = new ArrayList<>();
 
+        try {
+            File file = path.toFile();
+            if (file.exists()) {
+                Gson gson = new Gson();
+                Type shopListType = new TypeToken<ArrayList<Shop>>() {}.getType();
+
+                shops = gson.fromJson(new FileReader(file), shopListType);
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+        return shops;
+    }
     public static Shop findByName(String name) {
-        List<Shop> tiendas = cargarTiendasDesdeArchivo();
-        for (Shop tienda : tiendas) {
-            if (tienda.getName().equals(name)) {
-                return tienda;
+        List<Shop> shops = loadAllShops();
+
+        for (Shop shop : shops) {
+            if (shop.getName().equals(name)) {
+                return shop;
             }
         }
         return null;
     }
+
+
 }
