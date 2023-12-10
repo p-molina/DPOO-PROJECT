f2ed1f85ded16a8915b9f2ef271efa5c8a1ed108
Pablo Molina Bengochea <p.molina@students.salle.url.edu>
2023-12-10 19:48:35 +0100

    asdasd

--- src/Bussines/Entities/ShoppingCart.java
+++ src/Bussines/Entities/ShoppingCart.java
@@ -3,27 +3,49 @@ package Bussines.Entities;
 import java.util.ArrayList;
 import java.util.List;
 
+/**
+ * Representa un carrito de compras, incluyendo una lista de productos del catálogo y el precio total de compra.
+ */
 public class ShoppingCart {
     private List<CatalogProduct> catalogProductList;
     private double totalBuyPrice;
 
+    /**
+     * Constructor para inicializar el carrito de compras.
+     */
     public ShoppingCart() {
         this.catalogProductList = new ArrayList<>(0);
         this.totalBuyPrice = 0;
     }
-
+    /**
+     * Obtiene la lista de productos en el carrito de compras.
+     *
+     * @return Lista de productos del catálogo en el carrito.
+     */
     public List<CatalogProduct> getCatalogProductList() {
         return catalogProductList;
     }
-
+    /**
+     * Obtiene el precio total de los productos en el carrito de compras.
+     *
+     * @return Precio total de compra.
+     */
     public double getTotalBuyPrice() {
         return totalBuyPrice;
     }
-
+    /**
+     * Establece la lista de productos en el carrito de compras.
+     *
+     * @param catalogProductList Lista de productos del catálogo a establecer.
+     */
     public void setCatalogProductList(List<CatalogProduct> catalogProductList) {
         this.catalogProductList = catalogProductList;
     }
-
+    /**
+     * Establece el precio total de los productos en el carrito de compras.
+     *
+     * @param totalBuyPrice Precio total de compra a establecer.
+     */
     public void setTotalBuyPrice(double totalBuyPrice) {
         this.totalBuyPrice = totalBuyPrice;
     }
--- src/Presentation/CartController.java
+++ src/Presentation/CartController.java
@@ -2,12 +2,24 @@ package Presentation;
 
 import Bussines.ShoppingCartManager;
 
+/**
+ * Controlador para la gestión del carrito de compras en la interfaz de usuario.
+ */
 public class CartController {
     private ShoppingCartManager shoppingCartManager;
+
+    /**
+     * Constructor para inicializar el controlador del carrito de compras.
+     *
+     * @param shoppingCartManager Gestor del carrito de compras.
+     */
     public CartController(ShoppingCartManager shoppingCartManager)
     {
         this.shoppingCartManager = shoppingCartManager;
     }
+    /**
+     * Ejecuta la gestión del carrito de compras, mostrando su contenido y precio total.
+     */
     public void runManageCard()
     {
         String cardInfo = shoppingCartManager.getCardInfo();
