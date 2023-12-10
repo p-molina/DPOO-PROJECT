3314171f3d6af000b6b68f83cfb1eb5677f8ce83
Pablo Molina Bengochea <p.molina@students.salle.url.edu>
2023-12-10 19:45:33 +0100

    asdasd

--- src/Bussines/ShoppingCartManager.java
+++ src/Bussines/ShoppingCartManager.java
@@ -5,18 +5,34 @@ import Bussines.Entities.ShoppingCart;
 
 import java.util.List;
 
+/**
+ * Gestiona las operaciones del carrito de compras.
+ */
 public class ShoppingCartManager {
     private ShoppingCart shoppingCart;
 
+    /**
+     * Constructor para inicializar el gestor del carrito de compras.
+     */
     public ShoppingCartManager() {
         this.shoppingCart = new ShoppingCart();
     }
+    /**
+     * Agrega un producto al carrito de compras.
+     *
+     * @param catalogProduct Producto del catálogo a agregar al carrito.
+     */
     public void addProductToCard(CatalogProduct catalogProduct)
     {
         List<CatalogProduct> cardList = shoppingCart.getCatalogProductList();
         cardList.add(catalogProduct);
         shoppingCart.setCatalogProductList(cardList);
     }
+    /**
+     * Obtiene la información del carrito de compras, incluyendo los productos y el precio total.
+     *
+     * @return Cadena de texto con la información del carrito de compras.
+     */
     public String getCardInfo()
     {
         String cardInfo = "\nYour card contains the following items:";
